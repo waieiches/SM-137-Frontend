20ba83a37f1b1d95cf5a271df449fea62f02b603
hanheel <hanhee630630@gmail.com>
2024-09-26 12:02:43 +0900

    eslint / prettier / husky 적용

--- sm-137/src/App.tsx
+++ sm-137/src/App.tsx
@@ -1,9 +1,18 @@
+import styled from "@emotion/styled"
+
+const Wrap = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: #797979;
+`
+
 function App() {
 
   return (
-    <>
+    <Wrap>
       <h1>hi</h1>
-    </>
+
+    </Wrap>
   )
 }
 

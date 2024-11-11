cd0208abe80a7bc838c5a51b2ef8fc8d74ffb802
hanheel <hanhee630630@gmail.com>
2024-11-12 02:03:49 +0900

    feat : 카테고리 태그를 그룹으로 묶어 배열을 입력받도록 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import styled from "@emotion/styled";
 import CategoryTag from "./components/category-tag/CategoryTag";
 import "./reset.css";
+import CategoryTagGroup from "./components/category-tag/CategoryTagGroup";
 
 const Wrap = styled.div`
   display: flex;
+  flex-direction: column;
   gap: 0.5rem;
 `;
 
 function App() {
+  const mockTagArray = ["시설", "설비", "냉난방"];
   return (
     <Wrap>
       <CategoryTag contents="시설" />
-      <CategoryTag contents="설비" />
-      <CategoryTag contents="냉난방" />
+      <CategoryTagGroup tagArray={mockTagArray} />
     </Wrap>
   );
 }

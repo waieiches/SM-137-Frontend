acee8887756e3c988e0b6aeb88fddc638e221634
hanheel <hanhee630630@gmail.com>
2024-11-12 01:53:44 +0900

    feat: 카테고리 태그 레이아웃 제작

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import "./reset.css"
+import styled from "@emotion/styled";
+import CategoryTag from "./components/category-tag/CategoryTag";
+import "./reset.css";
+
+const Wrap = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`;
 
 function App() {
-  return
+  return (
+    <Wrap>
+      <CategoryTag contents="시설" />
+      <CategoryTag contents="설비" />
+      <CategoryTag contents="냉난방" />
+    </Wrap>
+  );
 }
 
 export default App;

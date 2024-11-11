176fbd199d2045cbc556f2b70555812ccbbf5a7e
hanheel <hanhee630630@gmail.com>
2024-11-12 01:14:31 +0900

    feat : 해시태그 및 선택 버튼 레이아웃 제작

---
+++ src/components/pill-button/PillButton.tsx
@@ -0,0 +1,43 @@
+import styled from "@emotion/styled";
+import { useState } from "react";
+
+interface PillButtonProps {
+  contents: string;
+}
+interface ContainerProps {
+  isClick: boolean;
+}
+const Container = styled.button<ContainerProps>`
+  padding: 0 1rem;
+  background-color: var(--white);
+  border-radius: 3rem;
+  border: ${(props) => props.isClick && "2px solid var(--light-primary)"};
+  color: ${(props) =>
+    props.isClick ? "var(--light-primary)" : "var(--gray5-lowText)"};
+  &:hover {
+    color: ${(props) => !props.isClick && "var(--gray5-header)"};
+  }
+  @media (max-width: 768px) {
+    padding: 0 0.8rem;
+    font-size: 0.875rem;
+  }
+  @media (max-width: 480px) {
+    padding: 0 0.5rem;
+    font-size: 0.75rem;
+  }
+`;
+const Contents = styled.div``;
+
+const PillButton = ({ contents }: PillButtonProps) => {
+  const [isClick, setIsClick] = useState(false);
+  const handleClick = () => {
+    setIsClick((prev) => !prev);
+  };
+  return (
+    <Container isClick={isClick} onClick={handleClick}>
+      <Contents>{contents}</Contents>
+    </Container>
+  );
+};
+
+export default PillButton;

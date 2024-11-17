75cb81e8f4e40f3beef9cb4f99ba63e5eb87ebc9
Chaejy <xhdrp@sookmyung.ac.kr>
2024-11-17 20:18:02 +0900

    feat: 버튼 컴포넌트

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,35 +1,65 @@
 import styled from "@emotion/styled";
-//typeScript 정의
-interface ButtonProps {
-  width?: string; // Button 컴포넌트의 props 타입 정의
-}
-interface ButtonContainerProps {
-  width?: string; // width가 선택적 속성임을 정의
-}
-const ButtonContainer = styled.div<ButtonContainerProps>`
-  /*flex 줄 */
-  display: flex;
-  align-items: center;
-  text-align: center;
-  gap: 1rem; //ButtonContainer 밑에 하위 컴포넌트가 2개일 경우
-  justify-content: center;
+import { buttonType } from "./ButtonType";
 
+const DEFAULT_CONTENT = "버튼 내용";
+const DEFAULT_TYPE = "default";
+
+interface ContentProps {
+  content?: string;
+  type: keyof typeof buttonType; 
+}
+
+interface StyleProps {
+  width?: string;
+  height?: string;
+  padding?: string;
+  backGround?: string;
+  hoverBackGround?: string;
+  color?: string;
+  borderRadius?: string; 
+}
+
+const ButtonContainer = styled.button<StyleProps>`
   width: ${(props) => props.width};
-  height: 100px; //컴포넌트의 특정 크기 - px
-  border-radius: 4px; //컴포넌트 라운딩 처리
-  background-color: var(--primary); // var() - index.css에 정의해둔 변수명
-`;
-const ButtonContents = styled.h2`
-  //h2 - 피그마 기준으로 Desktop - header2
-  color: white; // 폰트 색상
+  height: ${(props) => props.height};
+  background-color: ${(props) => props.backGround};
+  padding: ${(props) => props.padding};
+  border-radius: ${(props) => props.borderRadius || "4px"}; 
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  border: none;
+  cursor: pointer;
+  &:hover {
+    background-color: ${(props) => props.hoverBackGround};
+  }
 `;
 
-const Button = ({ width }: ButtonProps) => {
+const ButtonContents = styled.div<StyleProps>`
+  display: flex;
+  color: ${(props) => props.color};
+  text-align: center;
+`;
+
+const ButtonTemp = ({
+  content = DEFAULT_CONTENT,
+  type = DEFAULT_TYPE,
+}: ContentProps) => {
+  const buttonStyle = buttonType[type];
+
   return (
-    <ButtonContainer width={width}>
-      <ButtonContents>hi</ButtonContents>
+    <ButtonContainer
+      width={buttonStyle.width}
+      height={buttonStyle.height}
+      padding={buttonStyle.padding}
+      backGround={buttonStyle.backGround}
+      hoverBackGround={buttonStyle.hoverBackGround}
+      borderRadius={buttonStyle.borderRadius}
+    >
+      <ButtonContents color={buttonStyle.color}>{content}</ButtonContents>
     </ButtonContainer>
   );
 };
 
-export default Button;
+export default ButtonTemp;
+

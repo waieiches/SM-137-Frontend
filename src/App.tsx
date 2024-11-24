0698aa25acda851644cd1695ad5f9030f4311c55
hanheel <hanhee630630@gmail.com>
2024-11-24 16:00:45 +0900

    fix : contentBox 요소가 화면과 다르게 보이는 오류 해결

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
-function App(){
-  return;
+import ContentBox from "./components/content/ContentBox";
+import "./reset.css";
+
+function App() {
+  return <ContentBox type="large" />;
 }
+
+export default App;

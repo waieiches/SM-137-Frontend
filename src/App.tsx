ac562c4658856bdb2ca327fbef17257d18190a00
hanheel <hanhee630630@gmail.com>
2024-11-26 18:20:58 +0900

    design : recHashTag의 디자인을 변경한다
    1. pillButton의 padding 값 변경
    2. 최대 height 풀기
    3. RecHashTag가 요소들을 감싸도록 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import PillButton from "./components/pill-button/PillButton";
 import RecHashTag from "./components/rec-hashtag/RecHashTag";
 import "./reset.css";
 
 const App = () => {
-  return <RecHashTag />;
+  return (
+    <>
+      <PillButton contents="교육대학원" />
+      <RecHashTag />
+    </>
+  );
 };
 
 export default App;
--- src/components/rec-hashtag/RecHashTag.tsx
+++ src/components/rec-hashtag/RecHashTag.tsx
@@ -2,15 +2,21 @@ import styled from "@emotion/styled";
 import PillButton from "../pill-button/PillButton";
 
 const HashTagContainer = styled.div`
-  display: flex;
-  flex-wrap: wrap;
+  display: inline-flex;
+  flex-direction: column;
   gap: 0.6rem;
   padding: 1.5rem;
   background-color: var(--gray2-subbtn);
   border-radius: 500px;
   justify-content: center;
+  align-items: center;
   max-width: 627.34px;
-  max-height: 165px;
+`;
+const HashTagButton = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 1rem;
 `;
 
 const ListTitle = styled.span`
@@ -39,9 +45,11 @@ const RecHashTag = () => {
   return (
     <HashTagContainer>
       <ListTitle>추천 해시태그</ListTitle>
-      {hashTagMockData.hashtags.map((tag, index) => (
-        <PillButton key={index} contents={tag} />
-      ))}
+      <HashTagButton>
+        {hashTagMockData.hashtags.map((tag, index) => (
+          <PillButton key={index} contents={tag} />
+        ))}
+      </HashTagButton>
     </HashTagContainer>
   );
 };

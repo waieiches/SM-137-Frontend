a507408e271c5b8c822602ee9710154587d5c939
hanheel <hanhee630630@gmail.com>
2024-12-29 03:29:23 +0900

    chore : 댓글 관련 mockData 및 type 제작

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/home/Home";
+import Detail from "./pages/detail/Detail";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
       {
         path: "",
         element: <Home />,
+      },
+      {
+        path: "complaint-detail",
+        element: <Detail />,
       },
     ],
   },

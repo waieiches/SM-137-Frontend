d821135b990d059ce946d38f5c2f4afa0292d581
hanheel <hanhee630630@gmail.com>
2024-12-29 01:24:01 +0900

    feat : /complaint-view 주소에 페이지 연결

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/home/Home";
+import View from "./pages/view/View";
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
       {
         path: "",
         element: <Home />,
+      },
+      {
+        path: "complaint-view",
+        element: <View />,
       },
     ],
   },

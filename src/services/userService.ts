30b3244258dc0a4d7e1d77c44ee1d4906712191a
hanheel <hanhee630630@gmail.com>
2024-12-29 12:47:16 +0900

    feat : 백엔드 api 호출 함수 완성
    userServices, complaintServices 관련 api 호출 함수 제작 완료

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -14,9 +14,43 @@ export const modify = async (data) => {
 export const myPage = async () => {
   try {
     const response = await apiClient.get(`/v1/user`);
+    console.log(response); // 백엔드에서 전달된 데이터 확인
+    return response.data;
+  } catch (error) {
+    console.error("마이페이지 조회 중 에러 발생 :", error);
+    throw error;
+  }
+};
+
+export const myComplaint = async () => {
+  try {
+    const response = await apiClient.get(`/v1/user/complaint`);
+    console.log(response); // 백엔드에서 전달된 데이터 확인
     return response.data;
   } catch (error) {
     console.error("개인정보 수정 중 에러 발생 :", error);
+    throw error;
+  }
+};
+
+export const Result = async () => {
+  try {
+    const response = await apiClient.get(`/v1/user/result`);
+    console.log(response); // 백엔드에서 전달된 데이터 확인
+    return response.data;
+  } catch (error) {
+    console.error("결과 조회 중 에러 발생 :", error);
+    throw error;
+  }
+};
+
+export const myScrap = async () => {
+  try {
+    const response = await apiClient.get(`/v1/user/scrap`);
+    console.log(response); // 백엔드에서 전달된 데이터 확인
+    return response.data;
+  } catch (error) {
+    console.error("스크랩한 민원 로딩 중 에러 발생 :", error);
     throw error;
   }
 };

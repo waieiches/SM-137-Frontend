bf88ff769b62fb082ab1016f85cb3af1a9691ff9
hanheel <hanhee630630@gmail.com>
2024-12-29 11:29:42 +0900

    design : reset.css input 변경

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -3,7 +3,17 @@ import apiClient from "./apiClient";
 //path, post 데이터에 대해 타입 정의 필요
 export const modify = async (data) => {
   try {
-    const response = await apiClient.patch(`/v1/user/modify`, data);
+    const response = await apiClient.patch(import.meta.env.USER_MODIFY, data);
+    return response.data;
+  } catch (error) {
+    console.error("개인정보 수정 중 에러 발생 :", error);
+    throw error;
+  }
+};
+
+export const myPage = async () => {
+  try {
+    const response = await apiClient.get(`/v1/user`);
     return response.data;
   } catch (error) {
     console.error("개인정보 수정 중 에러 발생 :", error);

1bcc6af1ca84cd584860913dd70f5765088212e0
hasol11 <kangji09879@sookmyung.ac.kr>
2025-01-01 01:40:04 +0900

    chore: 불필요한 파일 삭제

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,8 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/home/Home";
 import Scrap from "./pages/scrap/Scrap";
-import Complaint from "./pages/complaint/Complaint";
-import Modify from "./pages/modify/Modify";
 
 export const router = createBrowserRouter([
   {
@@ -17,14 +15,6 @@ export const router = createBrowserRouter([
       {
         path: "my-scrap",
         element: <Scrap />,
-      },
-      {
-        path: "my-complaint",
-        element: <Complaint />,
-      },
-      {
-        path: "my-modify",
-        element: <Modify />,
       },
     ],
   },

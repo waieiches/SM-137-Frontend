ec29897057d3a224140cc5f072f91a407fd0b17b
hanheel <hanhee630630@gmail.com>
2025-01-09 19:48:13 +0900

    design : 민원 컴포넌트의 배치를 그리드로 수정하고 요소간의 gap을 조정한다

--- src/pages/my-complaint/Complaint.tsx
+++ src/pages/my-complaint/Complaint.tsx
@@ -4,7 +4,6 @@ import { mockData } from "../../mockData";
 import { motion } from "framer-motion";
 import SvgIcon, { SvgIconProps } from "@mui/material/SvgIcon";
 import ContactPageRoundedIcon from "@mui/icons-material/ContactPageRounded";
-import React from "react";
 import FilterBar from "./ComplaintFilterBar";
 
 const Container = styled.div`
@@ -12,57 +11,70 @@ const Container = styled.div`
   flex-direction: column;
   align-items: center;
   width: 100%;
-  margin-top: 3rem;
+  gap: 3rem;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const ComplaintIcon = styled(SvgIcon)<SvgIconProps>`
   color: var(--disabled-primary);
   width: 2rem;
   height: 2rem;
-  margin-top: 7rem;
+  margin-top: 2rem;
 `;
 
 const ComplaintTitle = styled.h2`
-  margin: 1rem 0;
+  color: var(--gray6-black);
 `;
 
-const ComplaintLine = styled.div`
-  width: 100%;
-  height: 1px;
-  background-color: var(--gray3-border);
-  margin: 5% 0 1.5rem;
-`;
-
-const ComplaintGrid = styled.div`
+const ComplaintContainer = styled.div`
   display: flex;
-  flex-wrap: wrap;
-  gap: 2rem;
   justify-content: center;
+  align-items: center;
+  border-top: 1px solid var(--gray3-border);
+  padding: 5rem;
+  transform: translateY(-5rem);
+  z-index: 0;
+  width: 100vw;
+`;
+
+//complaint를 시작점부터 배치
+const ComplaintGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 2rem;
+  justify-content: start;
 `;
 
 const Complaint = () => {
-  const [isPublic, setIsPublic] = React.useState(true);
-
   return (
     <Container>
-      <ComplaintIcon component={ContactPageRoundedIcon} />
-      <ComplaintTitle>내 민원</ComplaintTitle>
-      <ComplaintLine />
+      <TitleContainer>
+        <ComplaintIcon component={ContactPageRoundedIcon} />
+        <ComplaintTitle>내 민원</ComplaintTitle>
+      </TitleContainer>
 
-      <FilterBar isPublic={isPublic} setIsPublic={setIsPublic} />
+      <FilterBar />
 
-      <ComplaintGrid>
-        {mockData.map((item, index) => (
-          <motion.div
-            key={index}
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.3 }}
-          >
-            <ContentBox data={item} type="small" />
-          </motion.div>
-        ))}
-      </ComplaintGrid>
+      <ComplaintContainer>
+        <ComplaintGrid>
+          {mockData.map((item, index) => (
+            <motion.div
+              key={index}
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.3 }}
+            >
+              <ContentBox data={item} type="small" />
+            </motion.div>
+          ))}
+        </ComplaintGrid>
+      </ComplaintContainer>
     </Container>
   );
 };
